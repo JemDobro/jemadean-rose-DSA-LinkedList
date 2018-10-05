142f26208fe97fe8d50d7c92ead9ee597d311f1b
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-04 19:36:11 -0700

    mystery program done

--- mysteryprogram.js
+++ mysteryprogram.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//This takes a linked list, and starting at the beginning, looks at the value of the first node, and then, starting one node forward, checks the remaining nodes forward looking for a matching value(looking for duplicate values).  If a matching value is found, it skips over it to check the remaining values.  Once it has completed that for the first node, it will move to the second node, then the thid, etc until the end of the list. 
+//This takes a linked list, and starting at the beginning, looks at the value of the first node, and then, starting one node forward, checks the remaining nodes forward looking for a matching value(looking for duplicate values).  If a matching value is found, it skips over it to check the remaining values.  Once it has completed that for the first node, it will move to the second node, then the thid, etc until the end of the list.  Runtime is polynomial O(n^2).
 
 function WhatDoesThisProgramDo(lst){  
   let current = lst.head;  
